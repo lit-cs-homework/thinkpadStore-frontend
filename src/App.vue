2439446a72b4b833897d7397370f8ddc2ed7009f
kangya87 <142956649+kangya87@users.noreply.github.com>
2025-11-23 18:48:23 +0800

    LogoCreate

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <StoreHome/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StoreHome from './components/StoreHome.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    StoreHome
   }
 }
 </script>
